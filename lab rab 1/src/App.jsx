d767b926b5451ab55803f573200baad51db2f9cc
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-10-19 22:16:42 +0300

    add content component

--- lab rab 1/src/App.jsx
+++ lab rab 1/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/header/header"
 import Menu from "./components/menu/menu"
+import Content from "./components/content/content"
 
 function App() {
   return (
     <>
       <Header></Header>
       <Menu></Menu>
+      <Content></Content>
     </>
   )
 }
---
+++ lab rab 1/src/components/content/content.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Content extends Component {
+  render() {
+    return (
+      <section style={{ padding: '20px', backgroundColor: '#f4f4f4', marginBottom: '20px' }}>
+        <h2>Main Content Area</h2>
+        <p>This is where the main content will go.</p>
+      </section>
+    );
+  }
+}
+
+export default Content;
