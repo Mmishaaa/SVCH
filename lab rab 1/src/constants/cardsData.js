92569e5e795e6de912d86e50f5a7859d63c8d307
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-10-19 22:36:37 +0300

    add cardsData constant

---
+++ lab rab 1/src/constants/cardsData.js
@@ -0,0 +1,10 @@
+const cardsData = [
+  { id: 1, title: 'Card 1', content: 'This is the content for card 1.' },
+  { id: 2, title: 'Card 2', content: 'This is the content for card 2.' },
+  { id: 3, title: 'Card 3', content: 'This is the content for card 3.' },
+  { id: 4, title: 'Card 4', content: 'This is the content for card 4.' },
+  { id: 5, title: 'Card 5', content: 'This is the content for card 5.' },
+  { id: 6, title: 'Card 6', content: 'This is the content for card 6.' },
+];
+
+export default cardsData;
