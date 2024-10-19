a271ebf36aa0d2b3fe5ffa5b8af3857c3999e174
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-10-19 23:07:30 +0300

    add cards props for cardsList

--- lab rab 1/src/components/cardsList/cardsList.jsx
+++ lab rab 1/src/components/cardsList/cardsList.jsx
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import cardsData from "../../constants/cardsData";
 import Card from "../card/card";
 
 class CardsList extends Component {
@@ -7,7 +6,7 @@ class CardsList extends Component {
     return(
       <section>
         <h2 style={{textAlign: 'center'}}>Cards List</h2>
-        {cardsData.map(card => (
+        {this.props.cards.map(card => (
           <Card key={card.id} title={card.title} content={card.content} />
         ))}
       </section>
--- lab rab 1/src/components/content/content.jsx
+++ lab rab 1/src/components/content/content.jsx
@@ -1,11 +1,11 @@
 import { Component } from "react";
 import CardsList from "../cardsList/cardsList";
-
+import cardsData from "../../constants/cardsData"
 class Content extends Component {
   render() {
     return (
       <section style={{ padding: '20px', backgroundColor: '#f4f4f4'}}>
-        <CardsList></CardsList>
+        <CardsList cards={cardsData}></CardsList>
       </section>
     );
   }
