4627c6542d95fd60b09560eb407f4e594371b725
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-10-22 21:27:20 +0300

    add search

--- labrab2/src/components/cardsList.jsx
+++ labrab2/src/components/cardsList.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./card";
-import { Box } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 
 function CardsList({ cards, onDelete }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredCards = cards.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    item.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-      {cards.map((item) => (
-        <Card key={item.id} item={item} onDelete={onDelete} />
-      ))}
+    <Box sx={{ padding: 2 }}>
+      <TextField
+        label="Search by Name or Description"
+        variant="outlined"
+        fullWidth
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        sx={{ marginBottom: 2 }}
+      />
+      <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+        {filteredCards.map((item) => (
+          <Card key={item.id} item={item} onDelete={onDelete} />
+        ))}
+      </Box>
     </Box>
   );
 }
