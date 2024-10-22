c157b7e20954b6568fcb64b60337a433d8c9d33e
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-10-22 23:43:54 +0300

    Update create.js

--- src/functions/create.js
+++ src/functions/create.js
@@ -2,19 +2,44 @@ import fs from 'node:fs/promises'
 import { v4 as uuidv4 } from 'uuid';
 import { Constants } from '../constants.js';
 
-const createAsync = async(user) => {
+const appendAsync = async(user) => {
   try {
     const userToCreate = {
       id: uuidv4(),
       ... user
     }
-    await fs.appendFile(Constants.DataUsersPath, JSON.stringify(userToCreate) + "\n");
+    await fs.appendFile(Constants.DataUsersPath, JSON.stringify(userToCreate) + ",\n");
     await fs.writeFile(`${Constants.DataUserPath}/user_${userToCreate.id}.json`, JSON.stringify(userToCreate))
   } catch (err) {
     console.log(err);
   }
 }
 
+const createAsync = async (user) => {
+  try {
+    const userToCreate = {
+      id: uuidv4(),
+      ...user
+    };
+
+    const jsonUsers = await fs.readFile(Constants.DataUsersPath, 'utf-8');
+    let users = [];
+
+    if (jsonUsers) {
+      users = JSON.parse(jsonUsers);
+    }
+
+    users.push(userToCreate);
+
+    await fs.writeFile(Constants.DataUsersPath, JSON.stringify(users, null, 2));
+
+    await fs.writeFile(`${Constants.DataUserPath}/user_${userToCreate.id}.json`, JSON.stringify(userToCreate, null, 2));
+    
+    console.log('User created successfully:', userToCreate);
+  } catch (err) {
+    console.error('Error creating user:', err);
+  }
+};
 
 const args = process.argv.slice(2);
 
